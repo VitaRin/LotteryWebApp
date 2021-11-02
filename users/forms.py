ac7c4467be701afad648afe131297b3fadbbe50d
Ninasilvi <vita.rinkauskaite@gmail.com>
2021-11-02 00:49:57 +0000

    Added input validation for email and password

--- users/forms.py
+++ users/forms.py
@@ -1,13 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import Required, Email, Length, EqualTo
 
 
 class RegisterForm(FlaskForm):
-    email = StringField()
-    firstname = StringField()
-    lastname = StringField()
-    phone = StringField()
-    password = PasswordField()
-    confirm_password = PasswordField()
-    pin_key = StringField()
+    email = StringField(validators=[Required(), Email()])
+    firstname = StringField(validators=[Required()])
+    lastname = StringField(validators=[Required()])
+    phone = StringField(validators=[Required()])
+    password = PasswordField(validators=[Required(), Length(min=6, max=12, message='Password must be between 6 and '
+                                                                                   '12 characters in length.')])
+    confirm_password = PasswordField(validators=[Required(), EqualTo('password', message='Both password fields '
+                                                                                         'must be equal!')])
+    pin_key = StringField(validators=[Required()])
     submit = SubmitField()
