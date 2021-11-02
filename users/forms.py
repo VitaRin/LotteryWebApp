8e19f461877c2eff079b06b7db3c528e6cd20b16
Ninasilvi <vita.rinkauskaite@gmail.com>
2021-11-02 18:41:21 +0000

    Finished input validation

--- users/forms.py
+++ users/forms.py
@@ -1,3 +1,4 @@
+import re
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import Required, Email, Length, EqualTo, ValidationError
@@ -5,10 +6,18 @@ from wtforms.validators import Required, Email, Length, EqualTo, ValidationError
 
 def character_check(form, field):
     excluded_chars = "?!'^+%&/()=}][{$#@<>"
+    chars_present = ""
     for char in field.data:
         if char in excluded_chars:
+            if char not in chars_present:
+                chars_present += char
+    if chars_present != "":
+        if len(chars_present) == 1:
             raise ValidationError(
-                f"Character {char} is not allowed.")
+                f"Character {chars_present} is not allowed.")
+        else:
+            raise ValidationError(
+                f"Characters {chars_present} are not allowed.")
 
 
 class RegisterForm(FlaskForm):
@@ -20,5 +29,17 @@ class RegisterForm(FlaskForm):
                                                                                    '12 characters in length.')])
     confirm_password = PasswordField(validators=[Required(), EqualTo('password', message='Both password fields '
                                                                                          'must be equal!')])
-    pin_key = StringField(validators=[Required()])
+    pin_key = StringField(validators=[Required(), Length(min=32, max=32, message='PIN key must be 32 '
+                                                                                 'characters long.')])
     submit = SubmitField()
+
+    def validate_password(self, password):
+        p = re.compile(r'(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*\W)')
+        if not p.match(self.password.data):
+            raise ValidationError("Password must contain at least 1 digit, 1 lowercase, 1 uppercase and "
+                                  "1 special character.")
+
+    def validate_phone(self, phone):
+        p = re.compile(r'\d{4}-\d{3}-\d{4}')
+        if not p.match(self.phone.data):
+            raise ValidationError("Please use the following phone format: XXXX-XXX-XXXX.")
