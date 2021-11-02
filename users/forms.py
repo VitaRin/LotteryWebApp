aab409cd925d3ea649f4d12dc3e88ba7b98befce
Ninasilvi <vita.rinkauskaite@gmail.com>
2021-11-02 01:45:43 +0000

    Added input validation for firstname and lastname

--- users/forms.py
+++ users/forms.py
@@ -1,12 +1,20 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import Required, Email, Length, EqualTo
+from wtforms.validators import Required, Email, Length, EqualTo, ValidationError
+
+
+def character_check(form, field):
+    excluded_chars = "?!'^+%&/()=}][{$#@<>"
+    for char in field.data:
+        if char in excluded_chars:
+            raise ValidationError(
+                f"Character {char} is not allowed.")
 
 
 class RegisterForm(FlaskForm):
     email = StringField(validators=[Required(), Email()])
-    firstname = StringField(validators=[Required()])
-    lastname = StringField(validators=[Required()])
+    firstname = StringField(validators=[Required(), character_check])
+    lastname = StringField(validators=[Required(), character_check])
     phone = StringField(validators=[Required()])
     password = PasswordField(validators=[Required(), Length(min=6, max=12, message='Password must be between 6 and '
                                                                                    '12 characters in length.')])
