63b3533203b33254b1040ae78ef27d1cdd138f78
Ninasilvi <vita.rinkauskaite@gmail.com>
2021-11-03 14:39:36 +0000

    [TEST] Changed HTTP response code pages

--- app.py
+++ app.py
@@ -23,27 +23,27 @@ def index():
 # ERROR PAGE VIEWS
 @app.errorhandler(400)
 def bad_request(error):
-    return render_template('400.html'), 400
+    return render_template('errors/400.html'), 400
 
 
 @app.errorhandler(403)
 def page_forbidden(error):
-    return render_template('403.html'), 403
+    return render_template('errors/403.html'), 403
 
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('404.html'), 404
+    return render_template('errors/404.html'), 404
 
 
 @app.errorhandler(500)
 def internal_error(error):
-    return render_template('500.html'), 500
+    return render_template('errors/500.html'), 500
 
 
 @app.errorhandler(503)
 def service_unavailable(error):
-    return render_template('503.html'), 503
+    return render_template('errors/503.html'), 503
 
 
 if __name__ == "__main__":
